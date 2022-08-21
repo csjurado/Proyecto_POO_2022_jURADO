71fd00c3520ca2bfe721fc322b9e323a6a83f42e
csjurado <carlos.santiago.jurado.bazan@gmail.com>
2022-08-20 21:01:02 -0500

    Menu de inicio creado, con sus 3 respectivos módulos falta diseño y crear los respectivos modulos

---
+++ src/Bodeguero.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Bodeguero extends JDialog{
+    public String nombre;
+    public String email;
+    public String celular;
+    public String direccion;
+    public String password;
+
+}
---
+++ src/inicio.java
@@ -0,0 +1,172 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class inicio extends JDialog{
+    private JComboBox modosCB;
+    private JButton ingresarBTN;
+    private JButton salirBTN;
+    private JTextField usuarioTF;
+    private JPasswordField passwordTF;
+    private JPanel panelInicio;
+
+    public inicio(JFrame parent) {
+
+        super(parent);
+        setTitle("Login");
+        setContentPane(panelInicio);
+        setMinimumSize(new Dimension(640,480));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        ingresarBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (modosCB.getSelectedItem().toString()=="CAJERO"){
+                    String email = usuarioTF.getText();
+                    String password = String.valueOf(passwordTF.getPassword());
+                    System.out.println("boton ok");
+                    user = getAuthenticationUser(email,password);
+                    if(user!= null){
+
+                        JFrame crud1 = new JFrame("CAJERO");
+                        crud1.setContentPane(new panelCajeros().cajeros);
+                        crud1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        crud1.pack();
+                        crud1.setVisible(true);
+
+                        dispose();
+                    }else {
+                        JOptionPane.showMessageDialog(inicio.this,"Email o Password incorrectos","ERROR",JOptionPane.ERROR_MESSAGE);
+                        Limpiar();
+                    }
+                }
+
+                if (modosCB.getSelectedItem().toString()=="BODEGUERO"){
+                    String email = usuarioTF.getText();
+                    String password = String.valueOf(passwordTF.getPassword());
+                    System.out.println("boton ok");
+                    bodeguero = getAuthenticationBodeguero(email,password);
+                    if(bodeguero!= null){
+
+                        JFrame crud2 = new JFrame("BODEGUEROS");
+                        crud2.setContentPane(new panelBodegueros().bodegueros);
+                        crud2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        crud2.pack();
+                        crud2.setVisible(true);
+                        dispose();
+                    }else {
+                        JOptionPane.showMessageDialog(inicio.this,"Email o Password incorrectos","ERROR",JOptionPane.ERROR_MESSAGE);
+                        Limpiar();
+                    }
+
+                }
+
+            }
+        });
+        setVisible(true);
+        salirBTN.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Limpiar();
+            }
+        });
+    }
+    public User user;
+    private User getAuthenticationUser(String email,String password){
+        User user = null;
+
+        final String DB_URL="jdbc:mysql://localhost/farmacia?serverTimezone=UTC";
+        final String USERNAME= "csjurado";
+        final String PASSWORD= "12345";
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt=conn.createStatement();
+            String sql="SELECT * FROM cajeros WHERE email=? AND password=?";
+            PreparedStatement preparedStatement=conn.prepareStatement(sql);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+            System.out.println("Conexion ok ");
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                user= new User();
+
+                user.nombre=resultSet.getString("nombre");
+                user.email=resultSet.getString("email");
+                user.celular=resultSet.getString("celular");
+                user.direccion=resultSet.getString("direccion");
+                user.password=resultSet.getString("password");
+            }
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            System.out.println("Error de conexion");
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public Bodeguero bodeguero;
+    private Bodeguero getAuthenticationBodeguero(String email,String password){
+        Bodeguero bodeguero = null;
+
+        final String DB_URL="jdbc:mysql://localhost/farmacia?serverTimezone=UTC";
+        final String USERNAME= "csjurado";
+        final String PASSWORD= "12345";
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            Statement stmt=conn.createStatement();
+            String sql="SELECT * FROM bodegueros WHERE email=? AND password=?";
+            PreparedStatement preparedStatement=conn.prepareStatement(sql);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+            System.out.println("Conexion ok ");
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                bodeguero= new Bodeguero();
+
+                bodeguero.nombre=resultSet.getString("nombre");
+                bodeguero.email=resultSet.getString("email");
+                bodeguero.celular=resultSet.getString("celular");
+                bodeguero.direccion=resultSet.getString("direccion");
+                bodeguero.password=resultSet.getString("password");
+            }
+            stmt.close();
+            conn.close();
+        }catch (Exception e){
+            System.out.println("Error de conexion");
+            e.printStackTrace();
+        }
+
+        return bodeguero;
+    }
+
+
+
+    public static void main(String[] args) {
+        inicio loginForm = new inicio(null);
+        User user = loginForm.user;
+        Bodeguero bodeguero =loginForm.bodeguero;
+
+        if(user!=null){
+
+            ImageIcon usuarioimg = new ImageIcon("src/images/user.png");
+            JOptionPane.showMessageDialog(null, "Atenticacion correcta :"+user.nombre +"\n Email : "+user.email+"\n Celular : "+user.celular +"\n Direccion : "+user.direccion +"\n Password : "+user.password,
+                    "ACCESO PERMITIDO", JOptionPane.INFORMATION_MESSAGE, usuarioimg);
+
+        }else{
+
+            System.out.println("Autenticacion fallida");
+        }
+    }
+    public void Limpiar(){
+        //usuarioTF.setText("");
+        passwordTF.setText("");
+    }
+
+}
